de73e3b225d6141ac82efa3818a282e11f3852df
itsdoanguen <doannguyennguyen654@gmail.com>
2025-03-13 09:37:44 +0700

    Adding login and signup viewModel

---
+++ Attributes/PasswordComplexityAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PBL3.Attributes
+{
+    public class PasswordComplexityAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Password is required");
+            }
+
+            var password = value.ToString();
+        
+            if (password.Length < 6 || password.Length > 18)
+            {
+                return new ValidationResult("The length of password must be greater than 6 and smaller than 18");
+            }
+            if (!password.Any(char.IsDigit))
+            {
+                return new ValidationResult("Password must contain at least one digit");
+            }
+            if (!password.Any(char.IsUpper))
+            {
+                return new ValidationResult("Password must contain at least one uppercase letter");
+            }
+            if (!password.Any(char.IsLower))
+            {
+                return new ValidationResult("Password must contain at least one lowercase letter");
+            }
+            if (!password.Any(ch => !char.IsLetterOrDigit(ch)))
+            {
+                return new ValidationResult("Password must contain at least one special character");
+            }
+            if (password.Contains(" "))
+            {
+                return new ValidationResult("Password must not contain any whitespace");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PBL3.Attributes;
+
+namespace PBL3.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email is required"), EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required"), DataType(DataType.Password), PasswordComplexity]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm Password is required"), DataType(DataType.Password), Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
