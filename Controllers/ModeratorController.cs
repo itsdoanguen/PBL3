081300ceb96a0aa3bfad9ea406feb3cf7e62277f
itsdoanguen <doannguyennguyen654@gmail.com>
2025-03-15 23:32:07 +0700

    adding authorize for user and moderator

--- Controllers/ModeratorController.cs
+++ Controllers/ModeratorController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PBL3.Controllers
 {
+    [Authorize(Roles = "Moderator")]
     public class ModeratorController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PBL3.Controllers
 {
+    [Authorize(Roles ="User")]
     public class UserController : Controller
     {
         public IActionResult Index()
