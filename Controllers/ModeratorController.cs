9ea631d2a3e4e54f14ded615642531e9b37c85c7
itsdoanguen <doannguyennguyen654@gmail.com>
2025-03-15 21:26:38 +0700

    updating authorizaion controller, also fixing the view

---
+++ Controllers/ModeratorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PBL3.Controllers
+{
+    public class ModeratorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
