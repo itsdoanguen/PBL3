816e1f652c71c376099fb9b6384346d42e17dab7
itsdoanguen <doannguyennguyen654@gmail.com>
2025-05-08 22:01:34 +0700

    chỉnh lại phần view của các error

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PBL3.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/404")]
+        public IActionResult PageNotFound()
+        {
+            ViewBag.LayoutPath = GetLayoutForCurrentUser();
+            ViewBag.UserRole = User.FindFirst(ClaimTypes.Role)?.Value;
+
+            return View("PageNotFound");
+        }
+
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            ViewBag.LayoutPath = GetLayoutForCurrentUser();
+            ViewBag.UserRole = User.FindFirst(ClaimTypes.Role)?.Value;
+
+            if (statusCode == 404)
+            {
+                return RedirectToAction("PageNotFound");
+            }
+
+            return View("Error");
+        }
+
+        private string GetLayoutForCurrentUser()
+        {
+            var userRole = User.FindFirst(ClaimTypes.Role)?.Value;
+            if (User.Identity.IsAuthenticated)
+            {
+                return userRole switch
+                {
+                    "Admin" => "~/Views/Shared/_AdminLayout.cshtml",
+                    "User" => "~/Views/Shared/_UserLayout.cshtml",
+                    _ => "~/Views/Shared/_ModeratorLayout.cshtml"
+                };
+            }
+
+            return "~/Views/Shared/_AuthenticationLayout.cshtml";
+        }
+
+    }
+}
