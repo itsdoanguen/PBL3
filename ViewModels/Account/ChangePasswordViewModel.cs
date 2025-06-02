0e8a0dd884e0e8439c1d6b5404df4c158d847236
itsdoanguen <doannguyennguyen654@gmail.com>
2025-06-03 01:39:03 +0700

    thêm phần forgot password và gửi về email cho user

---
+++ ViewModels/Account/ChangePasswordViewModel.cs
@@ -0,0 +1,9 @@
+namespace PBL3.ViewModels.Account
+{
+    public class ChangePasswordViewModel
+    {
+        public string OldPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
